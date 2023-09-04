cb2223fc1bf8b634087d791afbd532f345d128eb
Affiliate V Dev <affiliatevineet@gmail.com>
2023-09-04 16:20:44 +0530

    Changes 20230904

--- academic/application/controllers/FeeCollectionController.php
+++ academic/application/controllers/FeeCollectionController.php
@@ -65,17 +65,21 @@ class FeeCollectionController extends Zend_Controller_Action {
     }
 
     public function indexAction() {
-    	$this->view->action_name = 'Fees';
-    	$this->view->sub_title_name = 'Fee Structure';
+    	$this->view->action_name = 'Fees Collection';
+    	$this->view->sub_title_name = 'Fee Collection';
     	$this->accessConfig->setAccess("SA_ACAD_FEE_HEADS");
 
         $fee_collection_model = new Application_Model_FeeCollector();
+        $Form_validation = new Application_Model_FormValidation();
+        $collector_form = new Application_Form_FeeCollector();
 
-        $get_all_students = $fee_collection_model->get_student_record();
+        $this->view->form = $collector_form;
+
+        // $get_all_students = $fee_collection_model->get_student_record();
         // echo "<pre>"; print_r($get_all_students); exit;
 
     	// $academic_year_model = new Application_Model_AcademicYear();
-    	
+        
     	// $FeeCollection_form = new Application_Form_FeeCollection();
 
     	// $this->view->form = $FeeCollection_form;
@@ -83,22 +87,187 @@ class FeeCollectionController extends Zend_Controller_Action {
     	// $academic_year_dropdown = $academic_year_model->getDropDownList();
     }
 
-    // public function ajaxGetStudents() {
-    //     $this->_helper->layout->disableLayout();
-    //     $Coursetype_model = new Application_Model_FeeCollection();
-    //     if ($this->_request->isPost() && $this->getRequest()->isXmlHttpRequest()) {
-    //         $degree = $this->_getParam("session_id");
-    //         $result = $Coursetype_model->getRecordByDegree($degree);
-    //         $degree_model = new Application_Model_Degree();
-    //         foreach ($result as $key => $value) {
-    //             $result[$key]['degree_id'] = $degree_model->getRecord($value['degree_id'])['degree'];
-    //         }
-    //         $page = $this->_getParam('page', 1);
-    //         $paginator_data = array(
-    //             'page' => $page,
-    //             'result' => $result
-    //         );
-    //         $this->view->paginator = $this->_act->pagination($paginator_data);
-    //     }
-    // }
+    public function ajaxGetSessionAction() {
+        $this->_helper->layout->disableLayout();
+        if ($this->_request->isPost() && $this->getRequest()->isXmlHttpRequest()) {
+            $academic_year_id = $this->_getParam("academic_year_id");
+            // echo $academic_year_id; die;
+            $fee_collector_model = new Application_Model_FeeCollector();
+
+            $result = $fee_collector_model->getSessionOnYear($academic_year_id);
+            // echo "<pre>";print_r($result);exit;
+            echo '<option value="">Select</option>';
+            foreach ($result as $k => $val) {
+                echo '<option value="' . $val['id'] . '" >' . $val['session'] . '</option>';
+            }
+        }die;
+    }
+
+    public function ajaxGetAcademicIdAction() {
+        $this->_helper->layout->disableLayout();
+        if ($this->_request->isPost() && $this->getRequest()->isXmlHttpRequest()) {
+            $session = $this->_getParam("session");
+            // echo $academic_year_id; die;
+            $fee_collector_model = new Application_Model_FeeCollector();
+
+            $result = $fee_collector_model->getAdacemicIdOnSession($session);
+            // echo "<pre>";print_r($result);exit;
+            echo '<option value="">Select</option>';
+            foreach ($result as $k => $val) {
+                echo '<option value="' . $val['academic_year_id'] . '" >' . $val['short_code'] . '</option>';
+            }
+        }die;
+    }
+
+    public function ajaxGetStudentsAction() {
+        $this->_helper->layout->disableLayout();
+        if ($this->_request->isPost() && $this->getRequest()->isXmlHttpRequest()) {
+            // $degree = $this->_getParam("degree");
+            $year = $this->_getParam("year");
+            $session = $this->_getParam("session");
+            $batch = $this->_getParam("batch");
+            $fee_collector_model = new Application_Model_FeeCollector();
+
+            $result = $fee_collector_model->get_student_record($year, $session, $batch);
+            // echo "<pre>";print_r($result); exit;
+            $page = $this->_getParam('page', 1);
+            $paginator_data = array(
+                'page' => $page,
+                'result' => $result
+            );
+            $this->view->paginator = $this->_act->pagination($paginator_data);
+            // $result;
+            // echo "<pre>";print_r($this->view->paginator);exit;
+            // echo '<option value="">Select</option>';
+            // foreach ($result as $k => $val) {
+            //     echo '<option value="' . $val['academic_year_id'] . '" >' . $val['short_code'] . '</option>';
+            // }
+        }
+    }
+
+    public function payAction() {
+        $this->view->action_name = 'Fees Collection';
+        $this->view->sub_title_name = 'Fee Collection';
+        $this->accessConfig->setAccess("SA_ACAD_FEE_HEADS");
+
+        $this->view->stid = $_GET['stid'];
+        $this->view->total = $_GET['total'];
+    }
+
+    public function ajaxGetFeeStructureAction() {
+
+        $this->_helper->layout->disableLayout();
+
+        if ($this->_request->isPost() && $this->getRequest()->isXmlHttpRequest()) {
+
+            $academic_year_id = $this->_getParam("academic_year_id");
+
+            $structure_id = $this->_getParam("structure_id");    
+            $this->view->stid = $this->_getParam("stid");    
+            $this->view->total = $this->_getParam("total");    
+            $Academic_model = new Application_Model_Academic();
+            $FeeCategory_model = new Application_Model_FeeCategory();
+          // $academic_year_id = $Academic_model->getRecordsByDepartment($department)[0]['academic_year_id'];
+            $FeeHeads_model = new Application_Model_FeeHeads();
+
+            $FeeStructure_model = new Application_Model_FeeStructure();
+
+            $StructureItems_model = new Application_Model_FeeStructureItems();
+
+            $term_model = new Application_Model_TermMaster();
+
+            $TermItems_model = new Application_Model_FeeStructureTermItems();
+            $dept_details = new Application_Model_Department();
+            $dept_type_details = new Application_Model_DepartmentType();
+
+            $acad_details =  $Academic_model->getRecord($academic_year_id);
+
+            $department = $acad_details['department'];
+            $session = $acad_details['session'];
+            $department = !empty($department)?$department:exit;
+
+            $dept_type_id = $dept_details->getRecord($department)['department_type'];
+            $dept_type_id = !empty($dept_type_id)?$dept_type_id:exit;
+            
+            
+            if (!empty($structure_id)) {
+
+             $result = $TermItems_model->getItemRecords($structure_id);
+
+             $this->view->result = $result;
+
+             $result1 = $StructureItems_model->getStructureRecords($structure_id);
+
+             $this->view->result1 = $result1;
+             $academic_id  = $TermItems_model->getAcademicId($structure_id);
+             $terms_data = $term_model->getRecordByAcademicId($academic_year_id);
+
+             $degree_id = $Academic_model->getAcademicDegree($academic_year_id);
+             $this->view->term_data = $terms_data; 
+             $this->view->structure_id = $structure_id;
+             $Category_data = $FeeCategory_model->getCategory($degree_id['degree_id'],$session,$dept_type_id);
+
+             $this->view->Category_data = $Category_data;
+
+             $Feeheads_data = $FeeHeads_model->getFeeheads($degree_id['degree_id'],$session,$dept_type_id);
+
+
+
+             $this->view->Feeheads_data = $Feeheads_data;
+
+         } else {
+
+            if (!empty($academic_year_id)) {
+
+                    // $student_model = new Application_Model_StudentPortal();
+                $degree_id = $Academic_model->getAcademicDegree($academic_year_id);
+                $Category_data = $FeeCategory_model->getCategory($degree_id,$session,$dept_type_id);
+
+                $this->view->Category_data = $Category_data;
+
+                $Feeheads_data = $FeeHeads_model->getFeeheads($degree_id,$session,$dept_type_id);
+
+                $terms_data = $term_model->getRecordByAcademicId($academic_year_id);
+
+                $this->view->term_data = $terms_data; 
+
+                $this->view->structure_id = 0;
+
+                $this->view->Feeheads_data = $Feeheads_data;
+
+                    // $Electivecourse_model = new Application_Model_ElectiveCourseLearning();
+
+                    //  $electives = $Electivecourse_model->getDropDownList();
+
+                      // echo "<pre>";  print_r($Category_data);
+
+                 // echo "<pre>";  print_r($Feeheads_data);die;
+
+                    //$this->view->electives = $electives;
+
+            }
+
+        }
+
+    }
+}
+
+    function ajaxPayFeeStructureAction () {
+        $this->_helper->layout->disableLayout();
+
+        $stid = $_GET['stid'];
+        $total = $_GET['total'];
+
+        if ($this->_request->isPost() && $this->getRequest()->isXmlHttpRequest()) {
+            $fee_collector_model = new Application_Model_FeeCollector();
+            $fee_collector_model->insert($_POST);
+            $upd_data = array(
+                "total_paid" => $_POST['total_paid'],
+                "total_due" => $_POST['total_due']
+            );
+            // exit;
+            $fee_collector_model->update($upd_data, array('student_id=?' => $_POST['student_id']));
+            print_r($_POST); exit;
+        }
+    }
 }
--- academic/application/models/FeeCollector.php
+++ academic/application/models/FeeCollector.php
@@ -4,17 +4,53 @@ class Application_Model_FeeCollector extends Zend_Db_Table_Abstract {
 	public $student_table = 'erp_student_information';
 	protected $_id = 'id';
 
-	public function get_student_record() {
+	public function get_student_record($year, $session, $batch) {
 		$select = $this->_db->select();
-		$select->from('erp_student_information');
-		$select->join(array("academic_master"), "academic_master.academic_year_id=erp_student_information.academic_id");
+		$select->from("erp_student_information", array("erp_student_information.*", "student_id as stid"));
+		$select->join(array("academic_master"), "academic_master.academic_year_id=erp_student_information.year_id");
 		$select->join(array("department"), " department.id=academic_master.department");
 		$select->join(array("degree_info"), " degree_info.id=department.degree_id");
 		$select->joinleft(array("erp_fee_heads_master"), " erp_fee_heads_master.degree_id=degree_info.id AND erp_fee_heads_master.session_id=academic_master.session AND erp_fee_heads_master.degree_id=department.degree_id");
 		$select->joinleft(array("erp_fee_structure_term_items"), " erp_fee_structure_term_items.fee_heads_id=erp_fee_heads_master.feehead_id");
 		$select->joinleft(array("fee_collector"), " fee_collector.student_id=erp_student_information.student_id");
-		echo $select; die;
+		// $select->where('department.degree_id=?', $degree);
+		$select->where('erp_student_information.year_id=?', $year);
+		$select->where('academic_master.session=?', $session);
+		$select->where('erp_student_information.academic_id=?', $batch);
+		$select->group('erp_student_information.student_id');
 		$result = $this->getAdapter()->fetchAll($select);
 		return $result;
+	}
+
+	public function getSessionOnYear ($academic_year_id) {
+		$select = $this->_db->select();
+		$select->from('session_info');
+		$select->where("acad_year_id=?", $academic_year_id);
+		$result = $this->getAdapter()->fetchAll($select);
+        return $result;
+	}
+
+	public function getAdacemicIdOnSession ($session) {
+		$select = $this->_db->select();
+		$select->from('academic_master');
+		$select->where("session=?", $session);
+		$result = $this->getAdapter()->fetchAll($select);
+        return $result;
+	}
+
+	public function getStudents ($degree, $year, $session, $batch) {
+		$select = $this->_db->select();
+		$select->from('academic_master');
+		$select->where("session=?", $session);
+		$result = $this->getAdapter()->fetchAll($select);
+        return $result;
+	}
+
+	public function getTotalfee($stid) {
+		$select = $this->_db->select('total_due');
+		$select->from('fee_collector');
+		$select->where("student_id=?", $stid);
+		$result = $this->getAdapter()->fetchRow($select);
+        return $result;	
 	}
 }
